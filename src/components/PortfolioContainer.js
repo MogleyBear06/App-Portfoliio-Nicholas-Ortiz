76bac7f615be197ce1d7b4cf0ed3d52681b464ca
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-07-11 12:42:58 -0500

    removed video

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -39,7 +39,7 @@ export default function PortfolioContainer() {
       <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* <ParticleCanvas></ParticleCanvas> */}
       {/* <Sketch></Sketch> */}
-      <Video></Video>
+      {/* <Video></Video> */}
       <br></br>
       <br></br>
       <br></br>
