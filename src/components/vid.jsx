f3965b0ce05e7763f5f244567dd8bbb18ebf30cf
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-07-12 16:10:19 -0500

    removed video autoplay

--- src/components/vid.jsx
+++ src/components/vid.jsx
@@ -3,7 +3,7 @@ import video from '../assets/headerbg.mp4';
 const vid = () => {
     return (
       <div className='container justify-content-center'>
-        <video src= {video} autoPlay controls loop style={{ width: '100%', height: '300pt' }}/>
+        <video src= {video} controls loop style={{ width: '100%', height: '300pt' }}/>
     
           </div> 
     )
