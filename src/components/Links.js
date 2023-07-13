f7149ecb503d17498503fef66e3cf6b63120c7b1
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-07-13 15:14:19 -0500

    modified link responsiveness

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,20 +5,19 @@ import { BiLogoImdb } from "react-icons/bi";
 
 function Links() {
   return (
-    
     <div className="container">
       <div className="row justify-content-center">
-      <div className="col-1 d-flex justify-content-center">
+        <div className="col-4 col-sm-1 d-flex justify-content-center mb-3 mb-sm-0">
           <a href="https://www.linkedin.com/in/nicholasjortiz/">
-            <AiFillLinkedin  style={{ color: 'Blue', fontSize: '40px' }} />
+            <AiFillLinkedin style={{ color: 'Blue', fontSize: '40px' }} />
           </a>
         </div>
-        <div className="col-1 d-flex justify-content-center" >
+        <div className="col-4 col-sm-1 d-flex justify-content-center mb-3 mb-sm-0">
           <a href="https://github.com/MogleyBear06">
-            <AiFillGithub  style={{ color: 'white', fontSize: '40px' }}/>
+            <AiFillGithub style={{ color: 'white', fontSize: '40px' }} />
           </a>
         </div>
-        <div className="col-1 d-flex justify-content-center">
+        <div className="col-4 col-sm-1 d-flex justify-content-center">
           <a href="https://www.imdb.com/name/nm13929885/">
             <BiLogoImdb style={{ color: 'gold', fontSize: '40px' }} />
           </a>
