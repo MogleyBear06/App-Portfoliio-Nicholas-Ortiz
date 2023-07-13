a3fb6b9f918175cf4dd081e886bbbadd04a6b261
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-07-13 15:58:09 -0500

    modified navtabs to be more responsize

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,23 +3,23 @@ import React from 'react';
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <div className="container">
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-lg navbar-dark">
         <div className="navbar-brand" style={{ fontSize: "30pt" }}>
           Nicholas Ortiz
         </div>
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
             <li className="nav-item">
               <a
                 href="#about"
