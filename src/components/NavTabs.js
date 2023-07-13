6971f8f44714274586805cab550fd76ec0d41694
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-07-13 16:21:05 -0500

    added collapse button to navtabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
 
 function NavTabs({ currentPage, handlePageChange }) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
   return (
     <div className="container">
       <nav className="navbar navbar-expand-lg navbar-dark">
@@ -11,54 +14,55 @@ function NavTabs({ currentPage, handlePageChange }) {
         <Button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
+          onClick={() => setNavbarOpen(!navbarOpen)}
           aria-controls="navbarNav"
-          aria-expanded="false"
+          aria-expanded={navbarOpen}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </Button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a
-                href="#about"
-                onClick={() => handlePageChange('About')}
-                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-              >
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#portfolio"
-                onClick={() => handlePageChange('Portfolio')}
-                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-              >
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#contact"
-                onClick={() => handlePageChange('Contact')}
-                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-              >
-                Contact Me
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#resume"
-                onClick={() => handlePageChange('Resume')}
-                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
-        </div>
+        <Collapse in={navbarOpen}>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a
+                  href="#about"
+                  onClick={() => handlePageChange('About')}
+                  className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                >
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#portfolio"
+                  onClick={() => handlePageChange('Portfolio')}
+                  className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#contact"
+                  onClick={() => handlePageChange('Contact')}
+                  className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                  Contact Me
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#resume"
+                  onClick={() => handlePageChange('Resume')}
+                  className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Collapse>
       </nav>
     </div>
   );
