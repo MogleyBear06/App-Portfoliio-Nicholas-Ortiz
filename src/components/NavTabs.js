dd5550e01d39e67ba6329541a327dcaeb60278a4
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-07-13 15:53:45 -0500

    modified navtabs responsiveness

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,47 +3,62 @@ import React from 'react';
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <div className="container">
-  <nav className="navbar navbar-expand-lg"> 
-     <div className="navbar-brand" style={{fontSize: "30pt"}} >Nicholas Ortiz</div>
-     <ul className="nav nav-pills ml-auto" style={{ marginLeft: 'auto'}}>
-      <li>
-        <a 
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+      <nav className="navbar navbar-expand-lg">
+        <div className="navbar-brand" style={{ fontSize: "30pt" }}>
+          Nicholas Ortiz
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          About
-        </a>
-      </li>
-      <li>
-        <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li>
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact Me
-        </a>
-      </li>
-      <li>
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
-    </ul>
-    </nav>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a
+                href="#about"
+                onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+              >
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#portfolio"
+                onClick={() => handlePageChange('Portfolio')}
+                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+              >
+                Contact Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#resume"
+                onClick={() => handlePageChange('Resume')}
+                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
