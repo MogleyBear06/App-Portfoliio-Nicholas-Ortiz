3289810e92787b3cbe902804aa8927e7ba95899b
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-07-13 16:52:32 -0500

    modified navtabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,69 +1,47 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Collapse from 'react-bootstrap/Collapse';
-
 function NavTabs({ currentPage, handlePageChange }) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
-
   return (
     <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="navbar-brand" style={{ fontSize: "30pt" }}>
-          Nicholas Ortiz
-        </div>
-        <Button
-          className="navbar-toggler"
-          type="button"
-          onClick={() => setNavbarOpen(!navbarOpen)}
-          aria-controls="navbarNav"
-          aria-expanded={navbarOpen}
-          aria-label="Toggle navigation"
+  <nav className="navbar navbar-expand-lg"> 
+     <div className="navbar-brand" style={{fontSize: "30pt"}} >Nicholas Ortiz</div>
+     <ul className="nav nav-pills ml-auto" style={{ marginLeft: 'auto'}}>
+      <li>
+        <a 
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
-          <span className="navbar-toggler-icon"></span>
-        </Button>
-        <Collapse in={navbarOpen} className="custom-collapse">
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a
-                  href="#about"
-                  onClick={() => handlePageChange('About')}
-                  className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                >
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  href="#portfolio"
-                  onClick={() => handlePageChange('Portfolio')}
-                  className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-                >
-                  Portfolio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  href="#contact"
-                  onClick={() => handlePageChange('Contact')}
-                  className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                  Contact Me
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  href="#resume"
-                  onClick={() => handlePageChange('Resume')}
-                  className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-                >
-                  Resume
-                </a>
-              </li>
-            </ul>
-          </div>
-        </Collapse>
-      </nav>
+          About
+        </a>
+      </li>
+      <li>
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li>
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact Me
+        </a>
+      </li>
+      <li>
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+    </nav>
     </div>
   );
 }
