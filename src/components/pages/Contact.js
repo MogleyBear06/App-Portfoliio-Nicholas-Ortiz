b818c2b82512b0a5015a62d30ac61491d1807955
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-07-13 15:31:09 -0500

    modifed contact to be more responsive

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { validateEmail } from "../utlls/helpers";
 
 function Contact() {
-
   const [formState, setFormState] = useState({
     name: "",
     email: "",
@@ -14,7 +13,6 @@ function Contact() {
   const { name, email, message } = formState;
 
   function handleChange(e) {
-    
     if (e.target.name === "email") {
       const isValid = validateEmail(e.target.value);
       if (!isValid) {
@@ -41,95 +39,83 @@ function Contact() {
 
   return (
     <>
-      <h4 className="container" style={{ maxWidth: "800px"}}>Would you like to know more? Drop us a line and lets start a conversation!</h4>
-        <section>
-          <form className="container">
-            <div className="container mt-4">
+      <h4 className="container-md">Would you like to know more? Drop us a line and let's start a conversation!</h4>
+      <section>
+        <form className="container">
+          <div className="container mt-4">
             <div className="row justify-content-center">
-            <div className="col-5">
-              <label>
-                Name
-              </label>{" "}
-              <br></br>
-              <input
-                type="text"
-                defaultValue={name}
-                onBlur={handleChange}
-                name="Name"
-                placeholder="FirstName, Lastname"
-              />
+              <div className="col-12 col-sm-10 col-md-5">
+                <label>Name</label>
+                <br />
+                <input
+                  type="text"
+                  defaultValue={name}
+                  onBlur={handleChange}
+                  name="Name"
+                  placeholder="FirstName, Lastname"
+                />
               </div>
-              <br></br>
-          
-            <div className="col-5">
-              <label>
-                Email
-              </label>
-              <br></br>
-              <input
-                type="email"
-                defaultValue={email}
-                name="email"
-                onBlur={handleChange}
-                placeholder="johndoe@gmail.com"
-              />
+              <br />
+              <div className="col-12 col-sm-10 col-md-5">
+                <label>Email</label>
+                <br />
+                <input
+                  type="email"
+                  defaultValue={email}
+                  name="email"
+                  onBlur={handleChange}
+                  placeholder="johndoe@gmail.com"
+                />
+              </div>
             </div>
-            </div>
-            </div>
-            <br></br>
-            <div className="container">
+          </div>
+          <br />
+          <div className="container">
             <div className="row justify-content-center">
-            <div className="col-10">
-                        <label for="status"> Are you a potential client, collaborator or simply interested?</label> 
-                       
-                    <select className="col-2 text-center" placeholder="Select">
-                    
-                        <option> Client </option>
-                        <option> Collaborator </option>
-                        <option> Simply Interested </option>
-                    
-                    </select> 
-                    </div>
-            </div>
-            </div>
-            <br></br>
-            <div className="container">
-            <div className='row justify-content-center'>
-            <div className='col-10'>
-              <label htmlFor="Message">
-                {" "}
-                What are you interested in learning more about?
-              </label>
-              <br></br>
-              <textarea style={{ width: "500px", height: "200px" }}
-                name="Message"
-                defaultValue={message}
-                onBlur={handleChange}
-                placeholder="Please Enter Your Message"
-                rows="6"
-              />
-            </div>
-            </div>
-            </div>
-            <br></br>
-            {errorMessage && (
-              <div>
-                <p className="error-text">{errorMessage}</p>
+              <div className="col-12 col-sm-10 col-md-10">
+                <label htmlFor="status">Are you a potential client, collaborator, or simply interested?</label>
+                <select className="col-2 text-center" placeholder="Select">
+                  <option>Client</option>
+                  <option>Collaborator</option>
+                  <option>Simply Interested</option>
+                </select>
               </div>
-            )}
-            <div className="container">
-            <div className='row justify-content-center'>
-            <div className='col-10'>
-            <button type="submit" onSubmit={handleSubmit}>
-              Submit
-            </button>
             </div>
+          </div>
+          <br />
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-12 col-sm-10 col-md-10">
+                <label htmlFor="Message">What are you interested in learning more about?</label>
+                <br />
+                <textarea
+                  className="col-12 col-sm-10 col-md-10"
+                  name="Message"
+                  defaultValue={message}
+                  onBlur={handleChange}
+                  placeholder="Please Enter Your Message"
+                  rows="6"
+                />
+              </div>
             </div>
+          </div>
+          <br />
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
             </div>
-            
-          </form>
-          
-        </section>
+          )}
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-12 col-sm-10 col-md-10">
+                <button type="submit" onSubmit={handleSubmit}>
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </section>
     </>
   );
 }
